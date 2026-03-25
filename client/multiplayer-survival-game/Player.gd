95b87c172b6f2f9ac1410ba1e35f80a65e5e8ebb
Sung Hwang <sung.hwang.tech@gmail.com>
2026-03-25 01:11:09 -0700

    Refactored Player.gd code for preparing for the multiplayer mode.

--- client/multiplayer-survival-game/Player.gd
+++ client/multiplayer-survival-game/Player.gd
@@ -1,30 +1,38 @@
 extends CharacterBody2D
 
+var is_local_player = false
+
 const SPEED = 200.0
 @onready var sprite = $AnimatedSprite2D
 var last_direction = Vector2.DOWN
 
-func _physics_process(delta):
-	var direction = Vector2.ZERO
+func _physics_process(delta):	
+	if is_local_player:
+		var direction = get_user_input()
+		# Ensures that diagonal movement isn't faster than straight movement.
+		velocity = direction.normalized() * SPEED
+		move_and_slide()
+	else:	# For multiplayers.
+		pass
+
+	if velocity.length() > 0:
+		last_direction = velocity
+
+	update_animation(velocity)
+
+# Handles the user input.
+func get_user_input() -> Vector2:
+	var input_vector = Vector2.ZERO
 	if Input.is_action_pressed("ui_right"):
-		direction.x += 1
+		input_vector.x += 1
 	if Input.is_action_pressed("ui_left"):
-		direction.x -= 1
+		input_vector.x -= 1
 	if Input.is_action_pressed("ui_down"):
-		direction.y += 1
+		input_vector.y += 1
 	if Input.is_action_pressed("ui_up"):
-		direction.y -= 1
-
-	# Diagonal movement speed calibration
-	direction = direction.normalized()
-
-	velocity = direction * SPEED
-	move_and_slide()
-
-	if direction.length() > 0:
-		last_direction = direction
-
-	update_animation(direction)
+		input_vector.y -= 1
+	
+	return input_vector
 
 func update_animation(direction: Vector2):
 	var is_moving = direction.length() > 0
