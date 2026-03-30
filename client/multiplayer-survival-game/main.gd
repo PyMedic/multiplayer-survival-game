11a85d6aa2fb04c832fa9915f29ade88cc66a252
Sung Hwang <sung.hwang.tech@gmail.com>
2026-03-30 00:16:20 -0700

    Created a container called Managers. This container will contain different managers that controls different aspects in the game. Placed the GameManager under this new manager container. Added a GDScript for the Main scene.

---
+++ client/multiplayer-survival-game/main.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@onready var ui = $UI
+@onready var game_world = $GameWorld
+@onready var players = $Players
+@onready var managers = $Managers
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
